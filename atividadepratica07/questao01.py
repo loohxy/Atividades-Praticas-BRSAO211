ed0e5039b16ae020e6d56372109c8d9cbd45104a
loohxy <lorenafernandescarvalho536@gmail.com>
2026-01-22 23:03:32 -0300

    atividades práticas - 07

---
+++ atividadepratica07/questao01.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+def processar_logs_treinamento(arquivo_log):
+    try:
+        leitor = pd.read_csv(arquivo_log)
+        media = leitor['tempo_execucao'].mean()
+        desvio_padrao = leitor['tempo_execucao'].std()
+        return f"Media: {media}, Desvio Padrão: {desvio_padrao}"
+
+
+
+    except FileNotFoundError:
+        return "Arquivo não encontrado"
+    
+arquivo = input("Digite o nome do arquivo de log: ")
+print(processar_logs_treinamento(arquivo))   
